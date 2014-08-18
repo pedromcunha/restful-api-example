cdb5f007f8c70cb75d0a46f1efe8aa5a819ad5b5
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-08-18 14:21:43 -0400

    testing with heroku

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 var express = require('express'),
+	http = require('http'),
 	restful = require('node-restful'),
 	bodyParser = require('body-parser'),
 	methodOverride = require('method-override'),
-	port = number(process.env.PORT || 3000),
+	port = process.env.PORT || 3000,
+	uristring = process.env.MONGOLAB_URI ||
+				process.env.MONGOHQ_URL ||
+				'mongodb://localhost/restful';
 	mongoose = restful.mongoose;
 
 var app = express();
 	app.use(bodyParser());
 	app.use(methodOverride());
 
-mongoose.connect('mongodb://localhost/restful');
+mongoose.connect(uristring, function(err, res) {
+	if (err)
+		console.log('Error connecting to: '+uristring+'. ' + err);
+	else
+		console.log('Succeeded connected to: '+ uristring);
+});
 
 var ProductSchema = mongoose.Schema({
 	name: String,
